6f98c5b02fe1a19391abd690c4d51bc76aa36253
joyce wamocho <joywamo@gmail.com>
2024-12-09 13:56:18 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr.Chocolate", 32, "F", "brown", "shy", 1),
+(2, "Rowdy", 20, "F", "black", "aggressive" ,0),
+(3," Tabitha" ,42, "M", "white", "curious", 1),
+(4, "Sergeant Brown" ,18, "M", "white", "shy", 1),
+(5, "Melissa", 38, "M", "black", "calm", 0),
+(6, "Grinch", 22, "F", "brown", "shy", 1),
+(7, "Wendy", 70, "F", "white", "aggressive",0),
+(8, NULL, 26, "M", "black", "shy" ,1);
